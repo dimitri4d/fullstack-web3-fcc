75c08d0214429f9cf8b433daf8f1714858899396
Dimitri Frederick <dimitri4d@gmail.com>
2022-08-28 00:06:21 -0400

    nextjs frontend ts

---
+++ lesson-10-nextjs-smart-contract-lottery/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import LotteryEntrance from "../components/LotteryEntrance";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Smart Contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <LotteryEntrance />
+    </div>
+  );
+};
+
+export default Home;
